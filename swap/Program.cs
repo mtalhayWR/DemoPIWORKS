7ff85138e02dc7e56759f5520d477be1100aafb0
mtalhayWR <72935916+mtalhayWR@users.noreply.github.com>
2020-10-15 19:57:49 +0300

    Add files via upload

---
+++ swap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12, b = 13;
+            Console.WriteLine("Swap before a:" + a + "b:" + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("Swap after a:" + a + "b:" + b);
+        }
+    }
+}
